a667cf8f974082f077e6ac121b28f004c55201ea
Miguel Maldonado <maldesan@gmail.com>
2018-10-21 15:03:01 -0400

    finalish push

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,8 +6,8 @@ class Header extends React.Component {
             <section id="header">
                 <div className="inner">
                     <span className="icon major fa-cloud"></span>
-                    <h1>Welcome to <strong>EasyPay</strong>, a rewards program<br />
-                    for the community by the community.</h1>
+                    <h1>Welcome to <strong>EasyPay</strong>, an easy payment system with<br />
+                    a rewards program for the community by the community.</h1>
                     <p>A convenient and secure way you can trust to pay your bills<br />
                     The more you use EasyPay, the more rewards you can access.</p>
                     <ul className="actions">
